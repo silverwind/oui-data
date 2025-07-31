982f811dc53f57f632894956845bbbd47f280ba8
silverwind <me@silverwind.io>
2025-07-31 10:34:19 +0200

    fix issue

--- index.test.js
+++ index.test.js
@@ -2,6 +2,6 @@ import {readFileSync} from "node:fs";
 
 test("ouiData", () => {
   // json import in vitest is horribly slow, this is about 50 times faster
-  const ouiData = JSON.parse(readFileSync(new URL("index.json", import.meta.url)));
+  const ouiData = JSON.parse(readFileSync(new URL("index.json", import.meta.url), "utf8"));
   expect(ouiData["203706"]).toMatch(/^Cisco/i);
 });
